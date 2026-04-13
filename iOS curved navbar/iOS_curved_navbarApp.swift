d6d4732bc6bfc780f9fc30e58051fa2f9927f0e7
Jonathan <jonfertri357@gmail.com>
2026-04-13 16:45:15 -0500

    Initial Commit

---
+++ iOS curved navbar/iOS_curved_navbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_curved_navbarApp.swift
+//  iOS curved navbar
+//
+//  Created by Jonathan Triana on 13/04/26.
+//
+
+import SwiftUI
+
+@main
+struct iOS_curved_navbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
